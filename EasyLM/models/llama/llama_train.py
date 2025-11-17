3687ee774af20122d5b9e5d0219363ad61cb7e15
natalieabreu <46090339+natalieabreu@users.noreply.github.com>
2025-11-17 09:13:01 -0500

    Remove hardcoded wandb_entity value

--- EasyLM/models/llama/llama_train.py
+++ EasyLM/models/llama/llama_train.py
@@ -77,7 +77,7 @@ FLAGS, FLAGS_DEF = mlxu.define_flags_with_default(
 
     wandb_run_id='',
     wandb_project='',
-    wandb_entity='harvardml',
+    wandb_entity='',
     wandb_dir='SOO-LM/experiment_output/open_llama_7b',
     output_dir='',
     notes='',
