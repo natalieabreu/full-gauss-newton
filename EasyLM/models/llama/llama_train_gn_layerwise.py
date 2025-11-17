9f51eec4dbd582fac8254da9685e0dcff382de43
natalieabreu <46090339+natalieabreu@users.noreply.github.com>
2025-11-17 09:12:42 -0500

    anonymize

--- EasyLM/models/llama/llama_train_gn_layerwise.py
+++ EasyLM/models/llama/llama_train_gn_layerwise.py
@@ -96,8 +96,8 @@ FLAGS, FLAGS_DEF = mlxu.define_flags_with_default(
     start_tokens=0,
 
     wandb_project='',
-    wandb_entity='harvardml',
-    wandb_dir='/n/netscratch/kempner_barak_lab/Lab/nabreu/SOO-LM/experiment_output/open_llama_7b',
+    wandb_entity='',
+    wandb_dir='SOO-LM/experiment_output/open_llama_7b',
     output_dir='',
     notes='',
     logger=mlxu.WandBLogger.get_default_config(),
